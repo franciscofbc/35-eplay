367cb1ae17c477697964541819d2950fd72fc473
Francisco Berti da Cruz <fbc991@live.com>
2023-04-26 22:51:39 -0300

    ate aula 3

---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Imagem = styled.div`
+  width: 100%;
+  height: 560px;
+  display: block;
+  background-repeat: no-repeat;
+  background-size: cover;
+  font-weight: bold;
+  padding-top: 344px;
+`
+
+export const Titulo = styled.h2`
+  font-size: 32px;
+  max-width: 456px;
+`
+
+export const Precos = styled.p`
+  font-size: 24px;
+  margin-top: 24px;
+
+  span {
+    text-decoration: line-through;
+  }
+`
